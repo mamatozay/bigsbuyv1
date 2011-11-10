4318fb89a3671a478698113d3f27f5c9bee7cebe
mamatozay <mamatozay.enine@gmail.com>
2011-11-11 07:46:50 +0800

    beginning spree on bigsbuy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bigsbuyv1::Application.config.secret_token = '7a73d1607a01a291ef5c173f66591c8928a885d1193f795d05f9a8f7b27eaa3d52e68fccb109abe555ad78afe469ca1be8c3142ee01e30b868dbff5fb490412e'
